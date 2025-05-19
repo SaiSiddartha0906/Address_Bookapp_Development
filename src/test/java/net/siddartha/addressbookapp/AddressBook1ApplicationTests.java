8462ba2929f4c86d0f29b7df0942064b423428f2
SaiSiddartha0906 <saisiddartha09062004@gmail.com>
2025-05-19 12:54:31 +0530

    Main Class created

---
+++ src/test/java/net/siddartha/addressbookapp/AddressBook1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.siddartha.addressbookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBook1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
